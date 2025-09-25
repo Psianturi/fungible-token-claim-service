367f6f89e9b8762e9804b3af287e19b97813122f
Psianturi <posma2018@gmail.com>
2025-09-26 01:36:38 +0700

    fix sandbox config and CI deployment - use correct contract account

--- src/config.sandbox.ts
+++ src/config.sandbox.ts
@@ -1,10 +1,10 @@
 export const config = {
   networkId: "sandbox",
-  nodeUrl: "http://localhost:24278",
+  nodeUrl: "http://localhost:3030",
   walletUrl: "http://localhost:4000/wallet",
-  helperUrl: "https://helper.testnet.near.org",
+  helperUrl: "http://localhost:3000",
   explorerUrl: "http://localhost:9001/explorer",
-  masterAccount: "master.test.near",
+  masterAccount: "test.near",
   ftContract: "ft.test.near",
   numberOfKeys: 10,
 };
